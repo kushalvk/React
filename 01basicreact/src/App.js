3100a47c6bc2eca05ff1eccf1c7e07f3ac243ef7
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-06-30 18:58:07 +0000

    React By npm react application

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    // you can assess only one tagin return ( you can declare one or more tag in the one tag )
+    <>
+      <Chai />
+      <h1>Hello React...!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai() { // name of the function is compalsory first latter is capital
+    return(
+        <h2>Chai aur React</h2>
+    )
+}
+
+export default Chai
