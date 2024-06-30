8106fe8e3844c6086fef30094499c565248928a6
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-06-30 18:59:09 +0000

    React By vite application (vitejs.dev)

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    // you can assess only one tagin return ( you can declare one or more tag in the one tag )
+    <div> 
+      <Chai/>
+      <h1>React ..</h1>
+      <p>Test</p>
+    </div>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {  // name of the function is compalsory first latter is capital
+    return(
+        <h3>React is Ready</h3>
+    )
+}
+
+export default Chai
