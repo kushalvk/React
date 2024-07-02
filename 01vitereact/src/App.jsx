3788f7a3d6c30f00518bf63e8645b42bd1bded0c
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-07-02 20:33:33 +0000

    buid React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from "./chai"
 
 function App() {
 
+  const username = 'kushal';
+
   return (
     // you can assess only one tagin return ( you can declare one or more tag in the one tag )
     <div> 
       <Chai/>
-      <h1>React ..</h1>
+      <h1>React ..{username}</h1>
       <p>Test</p>
     </div>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,42 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+// not work
+// function MyApp() {
+//     return (
+//         <div>
+//             <h1>Custome App !</h1>
+//         </div>
+//     )
+// }
+
+// not work
+// const ReactElement = { 
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+// const anotherElement = ( // not work
+//     <a href="https://google.com" target='_blank'>Visit Google</a>
+// )
+
+const anotherUser = "VK";
+
+const ReactElement = React.createElement( // fixed sysntex by React (Works) all elements are complsry
+    'a',
+    {href: 'http://google.com', target: '_blenk'},
+    'click to go google ',
+    anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    ReactElement
   
 )
