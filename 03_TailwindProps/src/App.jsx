14e9d8800b839bf09e438dd363a494d31ad2e4e3
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-07-05 19:05:09 +0000

    React with Tailwind css and props

---
+++ 03_TailwindProps/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  // let myObj = {
+  //   name: "kushal",
+  //   age: 22
+  // }
+
+  // let newArr = [1, 2, 3];
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card userName="kushalvk" /*someObj= {newArr}*/ btnText="push me"/>
+      <Card userName="VK" /*btnText="Click me"*/ />
+    </>
+  )
+}
+
+export default App
