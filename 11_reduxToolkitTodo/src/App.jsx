1a01bc7db9a097ab7aef8a0c06475420376a7973
Kushal Vaghela <142973474+kushalvk@users.noreply.github.com>
2024-07-13 17:30:30 +0000

    Todo app Context api and much more

---
+++ 11_reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from "./components/addTodo"
+import Todos from "./components/Todos"
+
+function App() {
+
+  
+
+  return (
+    <>
+      <h1>Learn about redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
